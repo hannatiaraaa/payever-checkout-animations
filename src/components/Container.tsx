f1b5238dc5012e0e73937b32c6140413b0c4c35d
Hanna Tiara Andarlia <hannatiara@gmail.com>
2023-10-13 16:29:24 +0700

    Create Main Container

---
+++ src/components/Container.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import TopBar from './nav/TopBar';
+import DockSide from './nav/DockSide';
+import type { TTheme } from '@/types/theme.type';
+
+type ContainerProps = {
+  children?: React.ReactNode;
+  className?: string;
+  activeNav?: string;
+};
+
+type Props = ContainerProps & Partial<TTheme>;
+
+const Container = ({ theme, children, className, activeNav }: Props) => {
+  return (
+    <div className={`flex flex-col p-4 gap-4 ${className}`}>
+      <TopBar theme={theme} />
+      <div className='flex h-full gap-4'>
+        <DockSide
+          theme={theme}
+          activeNav={activeNav}
+        />
+        <div className='flex-1'>{children}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Container;
